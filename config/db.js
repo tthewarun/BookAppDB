78debcd43699219738b218a1dbd90a7beca2779a
tthewarun <poywarunporn@gmail.com>
2024-11-02 20:39:11 +0700

    edit

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+//เชื่อมต่อฐานข้อมูล
 
 const { Sequelize } = require('sequelize');
 
@@ -5,7 +6,5 @@ const sequelize = new Sequelize('book_db_bzrj', 'book_db_bzrj_user', 'VLVcF8YuLf
 host: 'dpg-cshjeuo8fa8c739ejau0-a',
 dialect: 'postgres',
 });
-//เชื่อมต่อฐานข้อมูล
+
 module.exports = sequelize;
-//เชื่อมต่อฐานข้อมูล
-//เชื่อมต่อฐานข้อมูล
